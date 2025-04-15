94c31b8a2a04466854407db9becdbfec4e9b2359
yoshizumi0809 <yoshizumi.finction@docomo.ne.jp>
2025-04-15 11:18:46 +0900

    現在のページが1のときに前のページに行けなくする

--- src/components/Pagenation.tsx
+++ src/components/Pagenation.tsx
@@ -5,7 +5,6 @@ import { PostListContext } from '../providers/PostListProvider.tsx';
 export default function Pagenation(){
 
     const {page, setPage} = useContext(PostListContext)
-
     const gotoPreviousPage = () =>{
         setPage(Math.max(1, page - 1));
     }
@@ -16,7 +15,7 @@ export default function Pagenation(){
 
     return(
         <SPagenation>
-            <button onClick={gotoPreviousPage}>« 前のページ</button>
+            <button onClick={gotoPreviousPage} disabled={page === 1}>« 前のページ</button>
             <button onClick={gotoNextPage}>次のページ »</button>
         </SPagenation>
     )
