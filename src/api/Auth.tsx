21727a024ead0f5895fcf92dccaecae694b9d285
yoshizumi0809 <yoshizumi.finction@docomo.ne.jp>
2025-04-22 18:23:41 +0900

    無駄を除去

--- src/api/Auth.tsx
+++ src/api/Auth.tsx
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 export const sign_in = async (user_id: string, pass: string) => {
   const url = `http://localhost:3001/auth?user_id=${user_id}&password=${pass}`;
-  console.log(url);
   const res = await axios.get(url);//urlにGETリクエストを送信し、
   console.log(res);
   return res
